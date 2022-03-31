bb1d495cee6277a37983e667f20baf99ffd28473
Aswin-Shaji <aswin2000.ak@gmail.com>
2022-03-31 17:51:49 +0530

    Modified basic structre of the app and styles.

---
+++ src/columns.js
@@ -0,0 +1,139 @@
+export const columns = [
+  {
+    title: "Backlog",
+    tasks: [
+      {
+        id: 1,
+        title: `Add discount code to checkout page`,
+        date: "Sep 14",
+        type: "Feature Request",
+      },
+      {
+        id: 2,
+        title: `Provide documentation on integrations`,
+        date: "Sep 12",
+      },
+      {
+        id: 3,
+        title: "Design shopping cart dropdown",
+        date: "Sep 9",
+        type: "Design",
+      },
+      {
+        id: 4,
+        title: `Add discount code to checkout page`,
+        date: "Sep 14",
+        type: "Feature Request",
+      },
+      {
+        id: 5,
+        title: `Test checkout flow`,
+        date: "Sep 15",
+        type: "QA",
+      },
+    ],
+  },
+  {
+    title: "In Progress",
+    tasks: [
+      {
+        id: 6,
+        title: "Design shopping cart dropdown",
+        date: "Sep 9",
+        type: "Design",
+      },
+      {
+        id: 7,
+        title: "Add discount code to checkout page",
+        date: "Sep 14",
+        type: "Feature Request",
+      },
+      {
+        id: 8,
+        title: "Provide documentation on integrations",
+        date: "Sep 12",
+        type: "Backend",
+      },
+    ],
+  },
+  {
+    title: "Blocked",
+    tasks: [
+      {
+        id: 17,
+        title: "Design shopping cart dropdown",
+        date: "Sep 9",
+        type: "Design",
+      },
+      {
+        id: 18,
+        title: "Add discount code to checkout page",
+        date: "Sep 14",
+        type: "Feature Request",
+      },
+      {
+        id: 19,
+        title: "Provide documentation on integrations",
+        date: "Sep 12",
+        type: "Backend",
+      },
+    ],
+  },
+  {
+    title: "Review",
+    tasks: [
+      {
+        id: 9,
+        title: `Provide documentation on integrations`,
+        date: "Sep 12",
+      },
+      {
+        id: 10,
+        title: "Design shopping cart dropdown",
+        date: "Sep 9",
+        type: "Design",
+      },
+      {
+        id: 11,
+        title: `Add discount code to checkout page`,
+        date: "Sep 14",
+        type: "Feature Request",
+      },
+      {
+        id: 12,
+        title: `Design shopping cart dropdown`,
+        date: "Sep 9",
+        type: "Design",
+      },
+      {
+        id: 13,
+        title: `Add discount code to checkout page`,
+        date: "Sep 14",
+        type: "Feature Request",
+      },
+    ],
+  },
+  {
+    title: "Done",
+    tasks: [
+      {
+        id: 14,
+        title: "Add discount code to checkout page",
+        date: `Sep 14`,
+        type: "Feature Request",
+      },
+      {
+        id: 15,
+        title: "Design shopping cart dropdown",
+        date: "Sep 9",
+        type: "Design",
+      },
+      {
+        id: 16,
+        title: "Add discount code to checkout page",
+        date: "Sep 14",
+        type: "Feature Request",
+      },
+    ],
+  },
+];
--- src/components/TaskCard.vue
+++ src/components/TaskCard.vue
@@ -4,16 +4,15 @@
       <p class="text-gray-700 font-semibold font-sans tracking-wide text-sm">
         {{ task.title }}
       </p>
+      <div class="flex mt-4 justify-around items-center">
+        <badge v-if="task.type" :color="badgeColor">{{ task.type }}</badge>
+      </div>
 
       <img
         class="w-6 h-6 rounded-full ml-3"
         src="https://pickaface.net/gallery/avatar/unr_sample_161118_2054_ynlrg.png"
         alt="Avatar"
       />
-    </div>
-    <div class="flex mt-4 justify-between items-center">
-      <span class="text-sm text-gray-600">{{ task.date }}</span>
-      <badge v-if="task.type" :color="badgeColor">{{ task.type }}</badge>
     </div>
   </div>
 </template>
@@ -32,14 +31,22 @@ export default {
   computed: {
     badgeColor() {
       const mappings = {
-        Design: "purple",
-        "Feature Request": "teal",
-        Backend: "blue",
-        QA: "green",
-        default: "teal",
+        Design: "#acddde",
+        "Feature Request": "#f7d8ba",
+        Backend: "#e1f8dc",
+        QA: "#fef8dd",
+        default: "#fef",
       };
       return mappings[this.task.type] || mappings.default;
     },
   },
 };
 </script>
+
+<style lang="scss" scoped>
+img {
+  width: 10%;
+  height: 10%;
+  border-radius: 50%;
+}
+</style>
